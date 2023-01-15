1c8dd0f34ee0e4976611294760c2f48bb6985aee
schlaeflea <105506035+schlaeflea@users.noreply.github.com>
2023-01-15 16:22:39 +0100

    Test

--- javas.js
+++ javas.js
@@ -1,19 +1,37 @@
-function validate()
+const form = document.getElementById(`form`);
+const username = document.getElementById(`username`);
+const email = document.getElementById(`email`);
+const password = document.getElementById(`password`);
+const password2 = document.getElementById(`password2`);
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+};
+/* function validate()
 {
-    var username=document.getElementById("Email").value;
+    var email=document.getElementById("email").value;
     var password=document.getElementById("password").value;
 
-    if(username=="user"&& password=="password")
-    {
+    if (!(email === "user@gmx.ch" && password === "12345678")) {
+        alert("login failed")
+    } else {
         alert("login succesfully");
         return false;
     }
-    else
-    {
-        alert("login failed")
+    if (password.length < 8) {
+        alert("Das Passwort muss mindestens 8 Zeichen lang sein!");
+    } else {
+        alert("login succesfully");
+        return false;
     }
-}
+    var jsonString = JSON.stringify(obj);
+    console.log(jsonString);
+} */
 
-let javascriptObj = {name: "John", age: 30};
-let jsonObj = JSON.stringify(javascriptObj);
-console.log(jsonObj);
