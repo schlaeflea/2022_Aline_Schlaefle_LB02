f3c2b54f6d307091efdf26eb0da4a5a4c47fec68
schlaeflea <105506035+schlaeflea@users.noreply.github.com>
2023-01-16 13:04:35 +0100

    Test

--- javas.js
+++ javas.js
@@ -1,37 +1,30 @@
-const form = document.getElementById(`form`);
-const username = document.getElementById(`username`);
-const email = document.getElementById(`email`);
-const password = document.getElementById(`password`);
-const password2 = document.getElementById(`password2`);
-
-form.addEventListener("submit", e => {
-    e.preventDefault();
-
-    validateInputs();
-});
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-};
-/* function validate()
+function validate()
 {
     var email=document.getElementById("email").value;
     var password=document.getElementById("password").value;
 
-    if (!(email === "user@gmx.ch" && password === "12345678")) {
+
+    if (email === "user@gmx.ch" && password === "12345678") {
+        alert("login succesfully");
+        return false;
+    } else {
         alert("login failed")
+    }
+
+    if (email.length < 3) {
+        alert("Email Adresse muss mindestens 3 Zeichen lang sein!");
     } else {
         alert("login succesfully");
         return false;
     }
+
     if (password.length < 8) {
         alert("Das Passwort muss mindestens 8 Zeichen lang sein!");
     } else {
         alert("login succesfully");
         return false;
     }
-    var jsonString = JSON.stringify(obj);
-    console.log(jsonString);
-} */
+
+}
+
 
